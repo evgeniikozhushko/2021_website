a5a46210645b9dec1d52d56d7111025b42b8a674
Jonathan Coe <jon@northinteractive.co>
2021-11-24 16:59:18 -0700

    THIS SUCKS SO BAD

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".work-content").hide();
+
+    $("#work .card").on("click",function(){
+        var showthis = $(this).data("show");
+        $(".work-content").fadeOut(300,function(){
+            $(".work-content").data("content",showthis).fadeIn(300);
+        });
+    });
+
+});
