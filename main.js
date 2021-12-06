748d32c5752ac667980ab5f7c08e1a0e602a008c
Jonathan Coe <jon@northinteractive.co>
2021-12-06 13:21:19 -0700

    Updates

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
-$(document).ready(function(){
-    $("#work .open").click(function(){
-        $(this).css({"width":"100%"});
-        $(this).children(".portfolio-item").slideDown();
-        
-        $(this).removeClass("open").addClass("close");
-        
-        $(".close").click(function(){
-            $(this).children(".portfolio-item").slideUp();
-            $(this).removeClass("close").addClass("open");
+function showWorkItems() {
+    $("#work .open").unbind("click").on("click",function(){
+        var $this = $(this);
+
+        $this.css({"width":"100%"});
+        $this.children(".headerspace").css({"height":"50px"});
+        $this.children(".portfolio-item").slideDown(150);
+        $this.removeClass("open").addClass("close");
+
+        closeWorkItems();
+    });
+}
+
+function closeWorkItems(){
+
+    $("#work .close").unbind("click").on("click",function(){
+        var $this = $(this);
+        $this.children(".headerspace").css({"height":"300px"});
+        $this.removeClass("close").addClass("open");
+        showWorkItems();
+        $this.children(".portfolio-item").slideUp(150,function(){
+            var w = $this.width()/3-80;
+            $this.css({"width":w+"px"});
         });
     });
+}
+
+$(document).ready(function(){
+    showWorkItems();
 });
